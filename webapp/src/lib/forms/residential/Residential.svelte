814d751b7176e775f5eb43d8108ee286ec3e1c3b
Rustum Zia <rzia@ip0af58173.int.uvm.edu>
2025-02-19 21:01:37 -0500

    [Residential] React to user selecting multi-unit vs single-unit

--- webapp/src/lib/forms/residential/Residential.svelte
+++ webapp/src/lib/forms/residential/Residential.svelte
@@ -1,55 +1,51 @@
 <script lang="ts">
 	import { superForm, defaults } from 'sveltekit-superforms';
     import { zod } from 'sveltekit-superforms/adapters';
-	import { Field, Control, Label, Description, FieldErrors, Fieldset, Legend } from 'formsnap';
-	import { schema, homeTypes, relation } from './schema.js';
+	import { Field, ElementField, Control, Label, Description, FieldErrors, Fieldset, Legend } from 'formsnap';
+	import { schema, homeTypes, relationSingleUnit, relationMultiUnit } from './schema.js';
 	import SuperDebug from 'sveltekit-superforms';
 
 	const form = superForm(defaults(zod(schema)));
 	const { form: formData, enhance } = form;
+
+    let relation = relationSingleUnit;
+    function updateRelationOptions() {
+        if ($formData.homeType == "single-family") {
+            relation = relationSingleUnit;
+        } else {
+            relation = relationMultiUnit;
+        }
+    }
 </script>
 
 <h2>Residential section</h2>
 
 <form use:enhance class="mx-auto flex max-w-md flex-col" method="POST">
-    <Field {form} name="homeType">
+    <Fieldset {form} name="homeType">
         <Description>What type of residential building?</Description>
         {#each homeTypes as type}
           <Control>
             {#snippet children({ props })}
-              <input {...props} type="radio" value={type.id} bind:group={$formData.homeType} />
+              <input {...props} type="radio" value={type.id} onchange={updateRelationOptions} bind:group={$formData.homeType} />
               <Label>{type.desc}</Label>
             {/snippet}
           </Control>
         {/each}
         <FieldErrors />
-	</Field>
+	</Fieldset>
 
-    <Field {form} name="multi-unit-question">
-        <Description>Select the option that best describes your relation to the residence</Description>
-        {#each relation as type}
-          <Control>
-            {#snippet children({ props })}
-              <input {...props} type="radio" value={type.id} bind:group={$formData.relation} />
-              <Label>{type.desc}</Label>
-            {/snippet}
-          </Control>
-        {/each}
-        <FieldErrors />
-	</Field>
-
-    <Field {form} name="relation">
-        <Description>Select the option that best describes your relation to the residence</Description>
-        {#each relation as type}
-          <Control>
-            {#snippet children({ props })}
-              <input {...props} type="radio" value={type.id} bind:group={$formData.relation} />
-              <Label>{type.desc}</Label>
-            {/snippet}
-          </Control>
-        {/each}
-        <FieldErrors />
-	</Field>
+    <Fieldset {form} name="relation">
+      <Description>Select the option that best describes your relation to the residence</Description>
+      {#each relation as type}
+        <Control>
+          {#snippet children({ props })}
+            <input {...props} type="radio" value={type.id} bind:group={$formData.relation} />
+            <Label>{type.desc}</Label>
+          {/snippet}
+        </Control>
+      {/each}
+      <FieldErrors />
+	</Fieldset>
 	<button>Submit</button>
 </form>
 <SuperDebug data={$formData} />
--- webapp/src/lib/forms/residential/schema.ts
+++ webapp/src/lib/forms/residential/schema.ts
@@ -6,13 +6,20 @@ export const homeTypes = [
     { id: "mixed-use", desc: "Multi-unit mixed use building" },
 ] as const;
 
-export const relation = [
+export const relationSingleUnit = [
     { id: "renter", desc: "I rent this building" },
     { id: "owner", desc: "I own this building" },
     { id: "bystander", desc: "I am a bystander, not an owner or tenant of the residence" },
-]
+] as const;
+
+export const relationMultiUnit = [
+    { id: "renter", desc: "I rent in this building" },
+    { id: "owner-internal", desc: "I own and reside in this building" },
+    { id: "owner-external", desc: "I own this building and reside elsewhere" },
+    { id: "bystander", desc: "I am a bystander, not an owner or tenant of the residence" },
+] as const;
 
 export const schema = z.object({
     homeType: z.enum(homeTypes.map(option => option.id)),
-    relation: z.enum(relation.map(option => option.id)),
+    relation: z.enum(relationSingleUnit.concat(relationSingleUnit).map(option => option.id)),
 });
