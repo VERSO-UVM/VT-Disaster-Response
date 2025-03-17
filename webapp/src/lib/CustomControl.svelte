ec1d68f7b502c56b12db2e5eb7e3435734c2dfb5
Rustum Zia <ziarustum@gmail.com>
2025-03-17 16:21:43 -0400

    Add CustomControl component
    See top of CustomControl.svelte for explanation

---
+++ webapp/src/lib/CustomControl.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	// This is a simple wrapper around the Formsnap Control component.
+	// Control on its own does not create an HTML element. So it is reccomended
+	// to wrap it to create consistent styling and fit our needs. The
+	// documentation provides an example for this which this component is
+	// based off with our own customization.
+	// See https://www.formsnap.dev/docs/components/control
+
+	import { Control } from "formsnap";
+	import type { ComponentProps } from "svelte";
+ 
+	let {
+		// Rename the children prop to childrenProp to avoid
+		// conflicts with the Control component
+		children: childrenProp,
+		...restProps
+	}: ComponentProps<typeof Control> & {
+	} = $props();
+</script>
+
+<Control {...restProps}>
+	{#snippet children({ props })}
+		<div class="control-input">
+			<!-- Forward the props to the children snippet -->
+			{@render childrenProp({ props })}
+		</div>
+	{/snippet}
+</Control>
