978147d381acdf36f2235f2baf3861e5f8cab481
Rustum Zia <ziarustum@gmail.com>
2025-03-20 19:36:40 -0400

    Rename CustomControl to ControlDiv
    I don't like that I called it "CustomControl" before, so before I
    proliferate this component everywhere I am going to change the name to
    something that is marginally more descriptive.

--- webapp/src/lib/forms/residential/Residential.svelte
+++ webapp/src/lib/forms/residential/Residential.svelte
@@ -18,7 +18,7 @@
 		habitability
 	} from './schema.js';
 	import SuperDebug from 'sveltekit-superforms';
-    import CustomControl from '$lib/CustomControl.svelte'
+    import ControlDiv from '$lib/ControlDiv.svelte'
 
 	const form = superForm(defaults(zod(schema)));
 	const { form: formData, enhance } = form;
@@ -33,11 +33,11 @@
 	}
 </script>
 
-<form use:enhance class="mx-auto flex max-w-md flex-col" method="POST">
+<form use:enhance method="POST">
 	<Fieldset {form} name="homeType">
 		<Description>What type of residential building?</Description>
 		{#each homeTypes as type}
-			<CustomControl>
+			<ControlDiv>
 				{#snippet children({ props })}
 					<input
 						{...props}
@@ -48,7 +48,7 @@
 					/>
 					<Label>{type.desc}</Label>
 				{/snippet}
-			</CustomControl>
+			</ControlDiv>
 		{/each}
 		<FieldErrors />
 	</Fieldset>
@@ -56,12 +56,12 @@
 	<Fieldset {form} name="relation">
 		<Description>Select the option that best describes your relation to the residence</Description>
 		{#each relation as type}
-			<CustomControl>
+			<ControlDiv>
 				{#snippet children({ props })}
 					<input {...props} type="radio" value={type.id} bind:group={$formData.relation} />
 					<Label>{type.desc}</Label>
 				{/snippet}
-			</CustomControl>
+			</ControlDiv>
 		{/each}
 		<FieldErrors />
 	</Fieldset>
@@ -69,12 +69,12 @@
 	<Fieldset {form} name="habitability">
 		<Description>On a scale from 1-5, describe the habitability of your home</Description>
 		{#each habitability as type}
-			<CustomControl>
+			<ControlDiv>
 				{#snippet children({ props })}
 					<input {...props} type="radio" value={type.id} bind:group={$formData.habitability} />
 					<Label>{type.desc}</Label>
 				{/snippet}
-			</CustomControl>
+			</ControlDiv>
 		{/each}
 		<FieldErrors />
 	</Fieldset>
