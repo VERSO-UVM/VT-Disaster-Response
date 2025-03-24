ab427dbb5916088b3bcc104a90efa88f20b034fa
Rustum Zia <ziarustum@gmail.com>
2025-03-24 14:42:49 -0400

    Port CommercialForm to Formsnap

--- webapp/src/lib/forms/commercial/CommercialForm.svelte
+++ webapp/src/lib/forms/commercial/CommercialForm.svelte
@@ -2,24 +2,37 @@
     import { superForm, defaults } from 'sveltekit-superforms';
     import { zod } from 'sveltekit-superforms/adapters';
     import { connections, schema, types } from "./schema";
+	import { Label, Description, FieldErrors, Fieldset } from 'formsnap';
+    import ControlDiv from '$lib/elements/ControlDiv.svelte'
 
-    const { form, enhance } = superForm(defaults(zod(schema)));
+	const form = superForm(defaults(zod(schema)));
+	const { form: formData, enhance } = form;
 </script>
 
 <form method="POST" use:enhance>
-    <label for="type">What type of building? </label>
-    <select name="type" bind:value={$form.type}>
+	<Fieldset {form} name="type">
+		<Description>What type of building?</Description>
         {#each types as type}
-            <option value={type} selected={$form.type == type}>{type}</option>
-        {/each}
-    </select>
+			<ControlDiv>
+				{#snippet children({ props })}
+					<input {...props} type="radio" value={type} bind:group={$formData.type}/>
+                    <Label>{type}</Label>
+				{/snippet}
+			</ControlDiv>
+		{/each}
+		<FieldErrors />
+	</Fieldset>
 
-    <label for="connection">Select the option that best describes your connection to the building </label>
-    <select name="connection" bind:value={$form.connection}>
+	<Fieldset {form} name="connection">
+		<Description>What type of building?</Description>
         {#each connections as connection}
-            <option value={connection} selected={$form.connection == connection}>{connection}</option>
-        {/each}
-    </select>
-
-    <button type="submit">Submit</button>
+			<ControlDiv>
+				{#snippet children({ props })}
+					<input {...props} type="radio" value={connection} bind:group={$formData.type}/>
+                    <Label>{connection}</Label>
+				{/snippet}
+			</ControlDiv>
+		{/each}
+		<FieldErrors />
+	</Fieldset>
 </form>
